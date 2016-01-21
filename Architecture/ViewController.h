0e6690c1d4a4c9449c1070f74955f0b7561d18c9
Hu, Peng <peng.hu@sap.com>
2016-01-21 16:24:30 +0800

    Initial Commit

---
+++ Architecture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Architecture
+//
+//  Created by Hu, Peng on 1/21/16.
+//  Copyright © 2016 Hu, Peng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Architecture/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Architecture
+//
+//  Created by Hu, Peng on 1/21/16.
+//  Copyright © 2016 Hu, Peng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
